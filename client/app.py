15f48648c7b55071f635ede44e38d4a47a62e84e
bhavani <bhavanipn.1999@gmail.com>
2025-08-08 19:35:55 +0530

    Modified the code to make it work fine. Added the functionality in each buttons

--- client/app.py
+++ client/app.py
@@ -12,11 +12,15 @@ def about():
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('project.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
